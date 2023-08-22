37e1caf36cc434c8ba5caf3b130f5058e2fe5c3e
zhenekg <73281930+zhenekg@users.noreply.github.com>
2023-08-22 09:54:32 +0300

    12

--- networkDjango/chknetwork/lanview/models.py
+++ networkDjango/chknetwork/lanview/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.urls import reverse
 
 
 class Hardware(models.Model):
@@ -9,6 +10,26 @@ class Hardware(models.Model):
     content = models.TextField(blank=True)
     Location = models.CharField(max_length=250)
     photo = models.ImageField(upload_to="photos")
+    cat = models.ForeignKey('Category', on_delete=models.PROTECT, null=True)
+    filial = models.ForeignKey('Filial', on_delete=models.PROTECT, null=True)
 
     def __str__(self):
         return self.title
+
+    def get_absolute_url(self):
+        return reverse('post', kwargs={'post_id', self.pk})
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=100, db_index=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Filial(models.Model):
+    name = models.CharField(max_length=100, db_index=True)
+    location = models.CharField(max_length=250, null=True)
+
+    def __str__(self):
+        return self.name
